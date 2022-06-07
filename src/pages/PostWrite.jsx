87b2c4f53785c647bdebe519295f96490de98b9a
HyeJun <solar3070@naver.com>
2022-06-08 05:30:08 +0900

    Add: Post 뷰 태그 리스트 추가

--- src/pages/PostWrite.jsx
+++ src/pages/PostWrite.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Form, Input, Button, InputNumber, Upload } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
+import React, { useEffect, useRef, useState } from "react";
+import { Form, Input, Button, InputNumber, Upload, Tag, Tooltip } from "antd";
+// import { TweenOneGroup } from "rc-tween-one";
+import { UploadOutlined, PlusOutlined } from "@ant-design/icons";
 import "./PostWrite.scss";
 
 const { TextArea } = Input;
@@ -16,10 +17,57 @@ const normFile = (e) => {
 };
 
 const PostWrite = () => {
-  const [componentDisabled, setComponentDisabled] = useState(true);
+  const [tags, setTags] = useState([]);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [editInputIndex, setEditInputIndex] = useState(-1);
+  const [editInputValue, setEditInputValue] = useState("");
+  const inputRef = useRef(null);
+  const editInputRef = useRef(null);
 
-  const onFormLayoutChange = ({ disabled }) => {
-    setComponentDisabled(disabled);
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus();
+    }
+  }, [inputVisible]);
+
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [inputValue]);
+
+  const handleClose = (removedTag) => {
+    const newTags = tags.filter((tag) => tag !== removedTag);
+    console.log(newTags);
+    setTags(newTags);
+  };
+
+  const showInput = () => {
+    setInputVisible(true);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputConfirm = () => {
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      setTags([...tags, inputValue]);
+    }
+
+    setInputVisible(false);
+    setInputValue("");
+  };
+
+  const handleEditInputChange = (e) => {
+    setEditInputValue(e.target.value);
+  };
+
+  const handleEditInputConfirm = () => {
+    const newTags = [...tags];
+    newTags[editInputIndex] = editInputValue;
+    setTags(newTags);
+    setEditInputIndex(-1);
+    setInputValue("");
   };
 
   return (
@@ -32,11 +80,6 @@ const PostWrite = () => {
           span: 14,
         }}
         layout="horizontal"
-        initialValues={{
-          disabled: componentDisabled,
-        }}
-        onValuesChange={onFormLayoutChange}
-        disabled={componentDisabled}
       >
         <Form.Item
           label="제목"
@@ -86,6 +129,69 @@ const PostWrite = () => {
         </Form.Item>
         <Form.Item label="높이">
           <InputNumber />
+        </Form.Item>
+        <Form.Item label="태그">
+          {tags.map((tag, index) => {
+            if (editInputIndex === index) {
+              return (
+                <Input
+                  ref={editInputRef}
+                  key={tag}
+                  size="small"
+                  className="tag-input"
+                  value={editInputValue}
+                  onChange={handleEditInputChange}
+                  onBlur={handleEditInputConfirm}
+                  onPressEnter={handleEditInputConfirm}
+                />
+              );
+            }
+            const isLongTag = tag.length > 20;
+            const tagElem = (
+              <Tag
+                className="edit-tag"
+                key={tag}
+                closable
+                onClose={() => handleClose(tag)}
+              >
+                <span
+                  onDoubleClick={(e) => {
+                    if (index !== 0) {
+                      setEditInputIndex(index);
+                      setEditInputValue(tag);
+                      e.preventDefault();
+                    }
+                  }}
+                >
+                  {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                </span>
+              </Tag>
+            );
+            return isLongTag ? (
+              <Tooltip title={tag} key={tag}>
+                {tagElem}
+              </Tooltip>
+            ) : (
+              tagElem
+            );
+          })}
+          {inputVisible && (
+            <Input
+              ref={inputRef}
+              type="text"
+              size="small"
+              className="tag-input"
+              value={inputValue}
+              onChange={handleInputChange}
+              onBlur={handleInputConfirm}
+              onPressEnter={handleInputConfirm}
+            />
+          )}
+          {!inputVisible && (
+            <Tag className="site-tag-plus" onClick={showInput}>
+              <PlusOutlined /> New Tag
+            </Tag>
+          )}
         </Form.Item>
         <Form.Item
           wrapperCol={{
