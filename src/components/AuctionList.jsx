f231778484e4c98ee8fc36087ffcf73d642d7f8d
hildakim <pinkredjohn@gmail.com>
2022-06-11 17:12:30 +0900

    Add: 중고글, 경매 API로 데이터 받기
    - 중고글과 경매글 데이터를 DB에서 받아오도록 연결
    - 로딩 스피너 추가 (리스트 페이지)

--- src/components/AuctionList.jsx
+++ src/components/AuctionList.jsx
@@ -1,44 +1,17 @@
 import Auction from "./Auction";
 import "./AuctionList.scss";
 
-const AuctionList = () => {
-  const AUCTION_DATA = [
-    {
-      auctionId: 1,
-      category: "책상",
-      title: "5년된 책상",
-      description: "책상은 책상책상",
-      imgSrc:
-        "https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F50e606b8f8ff41628b4440ca2a0017ef.jpg",
-    },
-    {
-      auctionId: 2,
-      category: "의자",
-      title: "중고 의자 팔아요~^^ 한 번 썼읍니다.",
-      description: "중고의자 팝니다. 검은색. 연식 3년.",
-      imgSrc:
-        "https://image.hanssem.com/hsimg/gds/368/773/773575_A1.jpg?v=20210624133711",
-    },
-    {
-      auctionId: 3,
-      category: "침대",
-      title: "2년된 침대",
-      description: "푹신푹신 합니다.. 만오처넌",
-      imgSrc:
-        "https://image.ajunews.com/content/image/2021/12/24/20211224145900660054.jpg",
-    },
-  ];
-
+const AuctionList = ({ data }) => {
   return (
     <div className="auction-list-section">
-      {AUCTION_DATA.map((item) => (
+      {data.map((item) => (
         <Auction
           key={item.auctionId}
           id={item.auctionId}
-          category={item.category}
+          category={item.categoryId}
           title={item.title}
-          description={item.description}
-          imgSrc={item.imgSrc}
+          description={item.desc}
+          imgSrc={item.imageUrl}
         />
       ))}
     </div>
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,75 +1,75 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Image, Tag, Divider } from 'antd';
 import { useParams, Link } from 'react-router-dom';
 import { Button, Avatar } from 'antd';
-import { WechatOutlined, UserOutlined, DollarCircleOutlined } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
 
 import './PostDetail.scss';
 
 const PostDetail = () => {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const POST_DATA = [
-		{ 
-			postId: 1, 
-			userId: 1,
-			username: 'aria',
-			category: '책상', 
-			title: '5년된 책상',
-			price: 15000,
-			description: '롸..', 
-			imgSrc: 'https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F50e606b8f8ff41628b4440ca2a0017ef.jpg',
-		}, 
-		{ 
-			postId: 2,
-			userId: 2,
-			username: '냥',
-			category: '의자', 
-			title: '1년된 의자', 
-			price: 15000,
-			description: '느아..', 
-			imgSrc: 'https://image.hanssem.com/hsimg/gds/368/773/773575_A1.jpg?v=20210624133711',
-		}, 
-		{ 
-			postId: 3, 
-			userId: 3,
-			username: '병아리',
-			category: '침대', 
-			title: '2년된 침대', 
-			price: 15000,
-			description: '끄아악..', 
-			imgSrc: 'https://image.ajunews.com/content/image/2021/12/24/20211224145900660054.jpg',
-		}, 
-	];
+  // const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState([]);
 
-  const currentPost = POST_DATA.filter((post) => post.postId === parseInt(id))[0];
+  const getPostData = async() => {
+		const response = await axios.get(`http://localhost:8080/post?postId=${id}`);
+		setPost(response.data);	
+		// setLoading(false);
+		//TODO: api에서 customer 정보가 추가되면 수정해야하는 부분 있음
+  };
+
+  useEffect(() => {
+		getPostData();
+  }, []);
+
+  const changeDateFormat = (date) => {
+    if (date) {
+      const [createDay] = date.split("T");
+			return createDay.replaceAll("-", "/");
+    }
+    return date;
+  }
+
+  const convertCategory = (categoryId) => {
+    const categList = ['모두', '책상', '의자', '침대', '서랍'];
+    return categList[categoryId];
+  };
 
   return (
     <div className="post-detail">
 			<div className="content-wrapper">
 				<Image
-					src={currentPost.imgSrc}
+					src={post.imageUrl}
 				/>
 				<div className="post-user-info">
 					<div className="avatar">
 						<Avatar shape="square" icon={<UserOutlined />} />
-						{currentPost.username}
+						{post.nickname}
 					</div>
 					<Button>
-						<Link to={`/chatroom/${currentPost.userId}`}>{currentPost.username} 님과 채팅하기</Link>
+						<Link to={`/chatroom/${post.customerId}`}>{post.nickname} 님과 채팅하기</Link>
 					</Button>
 				</div>
 
 				<Divider />
 
-				<Tag color="geekblue">{currentPost.category}</Tag>
-				<span className="post-title">{currentPost.title}</span>
+				<Tag color="geekblue">{convertCategory(post.categId)}</Tag>
+				<span className="post-title">{post.title}</span>
 
 				<br/>
 
 				<Tag color="default">가격</Tag>
-				<span className="post-price">{currentPost.price} 원</span>
+				<span className="post-price">{post.price}원</span>
 				
-				<p className="post-desc">{currentPost.description}</p>
+				<br/>
+
+				<Tag color="default">크기</Tag>
+				<span className="post-furniture-size">W{post.width} * D{post.depth} * H{post.height}</span>
+				
+				<p className="post-desc">{post.desc}</p>
+				<p className="post-create_at">{changeDateFormat(post.createAt)}</p>
 				<Divider />
       </div>
     </div>
