e732253eb29024ab24390f2ef2137bbcf417b81f
HyeJun <solar3070@naver.com>
2022-06-17 19:11:40 +0900

    feat: 카테고리별 post, auction 분류

--- src/components/AuctionList.jsx
+++ src/components/AuctionList.jsx
@@ -1,19 +1,32 @@
 import Auction from "./Auction";
 import "./AuctionList.scss";
 
-const AuctionList = ({ data }) => {
+const AuctionList = ({ data, id }) => {
   return (
     <div className="auction-list-section">
-      {data.map((item) => (
-        <Auction
-          key={item.auctionId}
-          id={item.auctionId}
-          category={item.categoryId}
-          title={item.title}
-          description={item.desc}
-          imgSrc={item.imageUrl}
-        />
-      ))}
+      {id
+        ? data
+            .filter((data) => data.categoryId === id)
+            .map((item) => (
+              <Auction
+                key={item.auctionId}
+                id={item.auctionId}
+                category={item.categoryId}
+                title={item.title}
+                description={item.desc}
+                imgSrc={item.imageUrl}
+              />
+            ))
+        : data.map((item) => (
+            <Auction
+              key={item.auctionId}
+              id={item.auctionId}
+              category={item.categoryId}
+              title={item.title}
+              description={item.desc}
+              imgSrc={item.imageUrl}
+            />
+          ))}
     </div>
   );
 };
