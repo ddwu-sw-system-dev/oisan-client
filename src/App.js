92aaa81d548c76610e3ae660b5e41a60a0be4188
hildakim <pinkredjohn@gmail.com>
2022-06-18 01:41:33 +0900

    Add: 경매글 쓰기, 수정, 삭제 기능
    - 경매글 쓰기 버튼 및 기능 추가
    - 내 경매글 수정, 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import OipayCharge from "./pages/OiPayCharge";
 import Login from "./pages/Login";
 import OiPayUsage from "./components/OiPayUsage";
 import Signup from "./pages/Signup";
+import AuctionWrite from "./pages/AuctionWrite";
+import AuctionEdit from "./pages/AuctionEdit";
 
 function App() {
   return (
@@ -39,6 +41,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/auction" element={<AuctionListPage />} />
         <Route path="/auction/:id" element={<AuctionDetail />} />
+        <Route path="/auction/write" element={<AuctionWrite />} />
+        <Route path="/auction/edit/:id" element={<AuctionEdit />} />
       </Routes>
     </BrowserRouter>
   );
