6cb49791d65efb00ae18d40936cff2c5f2124476
HyeJun <solar3070@naver.com>
2022-06-18 01:44:34 +0900

    feat: 오이페이 충전/사용 성공시 메세지 띄우기

--- src/components/OiPayUse.jsx
+++ src/components/OiPayUse.jsx
@@ -1,95 +1,92 @@
-import { Button, Checkbox, Form, Input, InputNumber } from 'antd';
-import { DollarOutlined } from '@ant-design/icons';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  InputNumber,
+  Divider,
+  message,
+} from "antd";
+import { DollarOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 const OiPayUse = (props) => {
-	const {
-		remain,
-		biddingPrice,
-	} = props;
-	const [form] = Form.useForm();
-	const {
-		getFieldValue,
-		validateFields,
-	} = form;
-	const formItemLayout = {
-		labelCol: {
-			span: 4,
-		},
-		wrapperCol: {
-			span: 8,
-		},
-	};
-	
-	const formTailLayout = {
-		labelCol: {
-			span: 4,
-		},
-		wrapperCol: {
-			span: 8,
-			offset: 4,
-		},
-	};
+  const { remain, biddingPrice } = props;
+  const [form] = Form.useForm();
+  const { getFieldValue, validateFields } = form;
+  const [customer, setCustomer] = useState();
 
-	const spendOipay = async (amount) => {
-		const response = await axios.post('http://localhost:8080/oipay/use', {
-			customerId: 6,
-			amount: amount,
-		});
+  useEffect(() => {
+    setCustomer(JSON.parse(sessionStorage.getItem("customer")));
+  }, []);
 
-		console.log("response", response.data);		
-	};
+  const spendOipay = async (amount) => {
+    await axios
+      .post("http://localhost:8080/oipay/use", {
+        customerId: customer.customerId,
+        amount: amount,
+      })
+      .then((response) => {
+        message.success(`${response.data.amount}원을 사용하였습니다.`);
 
-	const onCheck = async () => {
-		try {
-			const values = await validateFields();
+        customer.oiPay -= amount;
+        sessionStorage.removeItem("customer");
+        sessionStorage.setItem("customer", JSON.stringify(customer));
+        setCustomer(JSON.parse(sessionStorage.getItem("customer")));
+      });
+  };
 
-			const amount = getFieldValue(['price']);
-			console.log(amount);
+  const onCheck = async () => {
+    try {
+      const values = await validateFields();
 
-			const response = await spendOipay(amount);
-			console.log('Success:', values, response); 
+      const amount = getFieldValue(["price"]);
+      console.log(amount);
 
-		} catch (errorInfo) {
-			console.log('Failed:', errorInfo);
-		//실패 시 메시지 띄우기~
-		}
-    };
+      const response = await spendOipay(amount);
+      console.log("Success:", values, response);
+    } catch (errorInfo) {
+      message.success("사용에 실패하였습니다.");
+      console.log("Failed:", errorInfo);
+      //실패 시 메시지 띄우기~
+    }
+  };
 
-	const priceValidate = (input, value) => {
-		if (!value || value < 100) {
-			return Promise.reject(new Error(input.message));
-		}
-		return Promise.resolve();
-	};
+  const priceValidate = (input, value) => {
+    if (!value || value < 100) {
+      return Promise.reject(new Error(input.message));
+    }
+    return Promise.resolve();
+  };
 
   return (
-		<div className="oipay-use-section">
-			{remain ? <p>잔여금액{remain}</p> : null}
-			<Form form={form}>
-				<Form.Item
-					{...formItemLayout}
-					name="price"
-					label="금액"
-					initialValue={biddingPrice ? biddingPrice : 0}
-					rules={[
-						{
-							validator: priceValidate,
-							message: '100원 이상의 금액을 입력하세요',
-						}
-					]}
-				>
-					<InputNumber placeholder="금액을 입력하세요" />
-					{/* <Input type='number' placeholder="금액을 입력하세요" allowClear={true}/> */}
-				</Form.Item>
+    <div className="oipay-use-section">
+      <Divider>오이페이 사용하기</Divider>
+      {remain ? <p>잔여금액{remain}</p> : null}
+      <Form form={form}>
+        <Form.Item
+          name="price"
+          label="금액"
+          initialValue={biddingPrice ? biddingPrice : 0}
+          rules={[
+            {
+              validator: priceValidate,
+              message: "100원 이상의 금액을 입력하세요",
+            },
+          ]}
+        >
+          <InputNumber placeholder="금액을 입력하세요" />
+          {/* <Input type='number' placeholder="금액을 입력하세요" allowClear={true}/> */}
+        </Form.Item>
 
-				<Form.Item {...formTailLayout}>
-					<Button type="primary" onClick={onCheck} icon={<DollarOutlined />}>
-						사용하기
-					</Button>
-				</Form.Item>
-			</Form>
-		</div>
+        <Form.Item>
+          <Button type="primary" onClick={onCheck} icon={<DollarOutlined />}>
+            사용하기
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- src/pages/OiPayCharge.jsx
+++ src/pages/OiPayCharge.jsx
@@ -1,88 +1,102 @@
-import React, { useState }  from 'react';
-import PropTypes from 'prop-types';
-import { Form, Button, Divider, Input, Radio, Space, InputNumber } from 'antd';
-import { DollarOutlined } from '@ant-design/icons';
-import axios from 'axios';
-import OiPayUse from '../components/OiPayUse';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import {
+  Form,
+  Button,
+  Divider,
+  Input,
+  Radio,
+  Space,
+  InputNumber,
+  message,
+  Typography,
+} from "antd";
+import { DollarOutlined } from "@ant-design/icons";
+import axios from "axios";
+import OiPayUse from "../components/OiPayUse";
 import "./OiPayCharge.scss";
 
 const OipayCharge = () => {
-	const [form] = Form.useForm();
-	const {
-		getFieldValue,
-		validateFields,
-	} = form;
+  const [form] = Form.useForm();
+  const { getFieldValue, validateFields } = form;
+  const [customer, setCustomer] = useState();
 
+  useEffect(() => {
+    setCustomer(JSON.parse(sessionStorage.getItem("customer")));
+  }, []);
 
-	const chargeOipay = async (amount) => {
-		const response = await axios.post('http://localhost:8080/oipay/charge', {
-			customerId: 6,
-			amount: amount,
-		});
+  const chargeOipay = async (amount) => {
+    await axios
+      .post("http://localhost:8080/oipay/charge", {
+        customerId: customer.customerId,
+        amount: amount,
+      })
+      .then((response) => {
+        message.success(`${amount}원이 충전되었습니다.`);
 
-		console.log("response", response.data);		
-	};
+        customer.oiPay += amount;
+        sessionStorage.removeItem("customer");
+        sessionStorage.setItem("customer", JSON.stringify(customer));
+        setCustomer(JSON.parse(sessionStorage.getItem("customer")));
+      });
+  };
 
-	const onCheck = async () => {
-		try {
-			const values = await validateFields();
-			
-			//충전시키는 부분
-			const amount = getFieldValue(['price-group']);
-			console.log(amount);
-			const response = await chargeOipay(amount);
+  const onCheck = async () => {
+    try {
+      const values = await validateFields();
 
-			console.log('Success:', values, response);
-	
-		} catch (errorInfo) {
-			console.log('Failed:', errorInfo);
+      //충전시키는 부분
+      const amount = getFieldValue(["price-group"]);
+      console.log(amount);
+      const response = await chargeOipay(amount);
 
-		}
-	};
+      console.log("Success:", values, response);
+    } catch (errorInfo) {
+      console.log("Failed:", errorInfo);
+      message.error("충전에 실패하였습니다.");
+    }
+  };
 
-	return (
-		<div className="oipay-charge-section">
-			<Divider>오이페이 충전하기</Divider>
-			<Form form={form}>
-				<Form.Item
-					name="price-group"
-					rules={[
-						{
-							required: true,
-							message: '금액을 선택하세요',
-						}
-					]}
-				>
-					<Radio.Group
-						label="충전 금액"
-					>
-						<Space direction="vertical">
-							<Radio value={1000}>1,000원</Radio>
-							<Radio value={3000}>3,000원</Radio>
-							<Radio value={5000}>5,000원</Radio>
-							<Radio value={10000}>10,000원</Radio>
-							<Radio value={30000}>30,000원</Radio>
-							<Radio value={50000}>50,000원</Radio>
-							<Radio value={100000}>100,000원</Radio>
-							<Radio value={1000000}>1,000,000원</Radio>
-						</Space>
-					</Radio.Group>
-				</Form.Item>
+  return (
+    <div className="oipay-charge-section">
+      <Divider>오이페이 충전하기</Divider>
+      <Form form={form}>
+        <Form.Item
+          name="price-group"
+          rules={[
+            {
+              required: true,
+              message: "금액을 선택하세요",
+            },
+          ]}
+        >
+          <Radio.Group label="충전 금액">
+            <Space direction="vertical">
+              <Radio value={1000}>1,000원</Radio>
+              <Radio value={3000}>3,000원</Radio>
+              <Radio value={5000}>5,000원</Radio>
+              <Radio value={10000}>10,000원</Radio>
+              <Radio value={30000}>30,000원</Radio>
+              <Radio value={50000}>50,000원</Radio>
+              <Radio value={100000}>100,000원</Radio>
+              <Radio value={1000000}>1,000,000원</Radio>
+            </Space>
+          </Radio.Group>
+        </Form.Item>
 
-				<Form.Item>
-					<Button type="primary" onClick={onCheck} icon={<DollarOutlined />}>
-						충전하기
-					</Button>
-				</Form.Item>
-			</Form>
+        <Form.Item>
+          <Button type="primary" onClick={onCheck} icon={<DollarOutlined />}>
+            충전하기
+          </Button>
+        </Form.Item>
+      </Form>
 
-			<OiPayUse />
-		</div>
-	);
+      {/* 오이페이 사용하기 */}
+      <OiPayUse />
+    </div>
+  );
 };
 
-OipayCharge.propTypes = {
-
-};
+OipayCharge.propTypes = {};
 
 export default OipayCharge;
