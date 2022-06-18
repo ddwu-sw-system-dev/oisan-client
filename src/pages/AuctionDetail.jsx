8df8555ea7c05719f9f487531469e3449fc8c8eb
hildakim <pinkredjohn@gmail.com>
2022-06-18 12:25:44 +0900

    Add: 경매 상세페이지에 입찰내역 추가
    - 경매 상세페이지 하단에 입찰내역을 보여주는 부분 추가
    - 홈 슬라이드에 임의의 사진 추가

--- src/pages/AuctionDetail.jsx
+++ src/pages/AuctionDetail.jsx
@@ -1,18 +1,36 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Image, Tag, Divider, Button, Avatar, Modal, Form, InputNumber, message } from "antd";
+import { Image, Tag, Divider, Button, Avatar, Modal, Form, InputNumber, message, Spin, Typography, List, Pagination } from "antd";
 import { useParams, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, LoadingOutlined } from '@ant-design/icons';
 import "./AuctionDetail.scss";
 // import useExist from '../hooks/useExist';
+const { Text } = Typography;
+
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 24,
+    }}
+    spin
+  />
+);
 
 const AuctionDetail = () => {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
   const [isMyPost, setIsMyPost] = useState(false);
   const [auction, setAuction] = useState([]);
   const [loginCustomer, setloginCustomer] = useState();
+  const [biddingList, setBiddingList] = useState([]);
   const navigate = useNavigate();
+
+  const pageSize = 5;
+  const [totalPage, setTotalPage] = useState(0);
+  const [current, setCurrent] = useState(1);
+  const [minIndex, setMinIndex] = useState(0);
+  const [maxIndex, setMaxIndex] = useState(0);
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
@@ -37,6 +55,22 @@ const AuctionDetail = () => {
 		const response = await axios.get(`http://localhost:8080/auction/${id}`);
 		setAuction(response.data);
     console.log(response.data);
+
+    const biddingHistory = await axios.get(`http://localhost:8080/bidding/${id}`);
+		setBiddingList(biddingHistory.data);
+    console.log(biddingHistory.data);
+
+    setTotalPage(biddingHistory.data.length / pageSize);
+    setMinIndex(0);
+    setMaxIndex(pageSize);
+
+    setLoading(false);
+  };
+
+  const handleBiddingPageChange = (page) => {
+    setCurrent(page);
+    setMinIndex((page - 1) * pageSize);
+    setMaxIndex(page * pageSize);
   };
 
   useEffect(() => {
@@ -54,6 +88,7 @@ const AuctionDetail = () => {
       setIsMyPost(true);
     }
   }, [auction, loginCustomer]);
+
 
   const changeDateFormat = (date) => {
     if (date) {
@@ -156,58 +191,109 @@ const AuctionDetail = () => {
     </Modal>
   );
 
+  const mSecToDate = (milliseconds) => {
+    const date = new Date(milliseconds);
+    const convert = date.toDateString();
+    return convert;
+  };
+
+  const biddingHistorySection = (
+    <div>
+        <List
+          footer={
+            <div>
+              <Pagination
+                defaultCurrent={1}
+                pageSize={pageSize}
+                current={current}
+                total={biddingList.length}
+                onChange={handleBiddingPageChange}
+              />
+            </div>
+          }
+          header={
+            <p>입찰 내역</p>
+          }
+          bordered
+          dataSource={biddingList.sort((a, b) => {
+              if (a.createAt > b.createAt) return -1;
+              if (a.createAt < b.createAt) return 1;
+              return 0;
+            })}
+          renderItem={(item) => {
+            if (item) {
+              return (
+                <List.Item>
+                  <Typography.Text type="secondary">[입찰가]</Typography.Text>
+                  <span className="amount-text">{item.price}원</span>
+                  <Text code>{mSecToDate(item.createAt)}</Text>
+                </List.Item>
+              );
+            }
+          }}
+        />
+      </div>
+  );
 
   return (
-    <StyledRoot>
-      <StyledImage>
-        <Image className="auction-detail-image" src={auction.imageUrl} />
-      </StyledImage>
-      <div className="auction-info-section">
-        <Tag color="geekblue">{convertCategory(auction.categoryId)}</Tag>
-        <h3>{auction.title}</h3>
-				작성자 : {auction.customer && auction.customer.nickname}
-        <p>크기 : {auction.furniture && `W${auction.furniture.width} * D${auction.furniture.depth} * H${auction.furniture.height}`}</p>
-        <Divider />
-        <p>{auction.desc}</p>
-        <Divider />
-        <StyledTable>
-          <tbody>
-          <tr>
-            <td>시작일</td>
-            <td>{changeDateFormat(auction.createAt)}</td>
-          </tr>
-          <tr>
-            <td>마감일</td>
-            <td>{changeDateFormat(auction.closingTime)}</td>
-          </tr>
-          <tr>
-            <td>시작 가격</td>
-            <td>{auction.startBid}</td>
-          </tr>
-          <tr>
-            <td>현재 가격</td>
-            <td>{auction.winningBid}</td>
-          </tr>
-          </tbody>
-        </StyledTable>
-        <Divider />
-        {isMyPost ? (
-          <div className="my-auction-edit-delete-button-section">
-            <Button type="primary" onClick={editPost}>수정</Button>
-            <Button type="danger" onClick={deletePost}>삭제</Button>
+    <div className="auction-detail-section">
+      {loading ? <Spin indicator={antIcon} /> : (
+      <>
+        <StyledRoot>
+          <StyledImage>
+            <Image className="auction-detail-image" src={auction.imageUrl} />
+          </StyledImage>
+          <div className="auction-info-section">
+            <Tag color="geekblue">{convertCategory(auction.categoryId)}</Tag>
+            <h3>{auction.title}</h3>
+            작성자 : {auction.customer && auction.customer.nickname}
+            <p>크기 : {auction.furniture && `W${auction.furniture.width} * D${auction.furniture.depth} * H${auction.furniture.height}`}</p>
+            <Divider />
+            <p>{auction.desc}</p>
+            <Divider />
+            <StyledTable>
+              <tbody>
+              <tr>
+                <td>시작일</td>
+                <td>{changeDateFormat(auction.createAt)}</td>
+              </tr>
+              <tr>
+                <td>마감일</td>
+                <td>{changeDateFormat(auction.closingTime)}</td>
+              </tr>
+              <tr>
+                <td>시작 가격</td>
+                <td>{auction.startBid}</td>
+              </tr>
+              <tr>
+                <td>현재 가격</td>
+                <td>{auction.winningBid}</td>
+              </tr>
+              </tbody>
+            </StyledTable>
+            <Divider />
+            {isMyPost ? (
+              <div className="my-auction-edit-delete-button-section">
+                <Button type="primary" onClick={editPost}>수정</Button>
+                <Button type="danger" onClick={deletePost}>삭제</Button>
+              </div>
+            ) 
+            : <Button
+                onClick={showModal}
+              >
+                입찰하기
+              </Button>
+            }
+            {biddingModal}
+            
           </div>
-        ) 
-        : <Button
-            onClick={showModal}
-          >
-            입찰하기
-          </Button>
-        }
-        {biddingModal}
-        
-      </div>
-      
-    </StyledRoot>
+        </StyledRoot>
+        <div className="bidding-list-section">
+          {biddingHistorySection}
+        </div>
+      </>
+      )}
+    </div>
   );
 };
 
